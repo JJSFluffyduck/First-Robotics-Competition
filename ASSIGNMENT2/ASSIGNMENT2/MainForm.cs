d1fe34ea4327b2c2a2cb20e738c35715f820d786
xincami <38929524+xincami@users.noreply.github.com>
2018-05-03 10:54:58 +0930

    Add files via upload

---
+++ ASSIGNMENT2/ASSIGNMENT2/MainForm.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ASSIGNMENT2
+{
+    public partial class MainForm : Form
+    {
+        
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        //All ComboBox use this funtion
+        private  void ComboBox_SelectedValueChanged(object sender, EventArgs e)
+        {
+            var cb = ((ComboBox)sender);
+            cb.BackColor = Color.White;
+            switch (cb.SelectedIndex)
+            {
+                //Unknow
+                case 0:
+                    cb.BackColor = Color.Orange;
+                    break;
+                // ' Yes
+
+                case 1:
+                    cb.BackColor = Color.Green;
+                    break;
+                // ' No
+
+                case 2:
+                    cb.BackColor = Color.Red;
+                    break;
+            }
+        }
+
+        private void TeamNumberBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(Char.IsDigit(e.KeyChar) || e.KeyChar == (char)(Keys.Back)))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            this.HighGoalBox.SelectedIndex = 0;
+            this.LowGoalBox.SelectedIndex = 0;
+            this.MiddleBarBox.SelectedIndex = 0;
+            this.PickUpBallBox.SelectedIndex = 0;
+            this.CatchBallBox.SelectedIndex = 0;
+            this.AutoHighGoalBox.SelectedIndex = 0;
+            this.AutoLowGoalBox.SelectedIndex = 0;
+            this.PassBallBox.SelectedIndex = 0;
+            this.AutoForwardBox.SelectedIndex = 0;
+          
+        }
+
+    }
+}
